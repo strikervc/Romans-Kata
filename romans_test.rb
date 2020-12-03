3b45f20d1d5e073c7d356de5e3eb4c8a65806989
Victor Valdez <victor334_@hotmail.com>
2020-12-03 10:56:21 -0400

    roman range test added

--- romans_test.rb
+++ romans_test.rb
@@ -8,4 +8,11 @@ class RomansTest < Test::Unit::TestCase
         assert_equal "All right", validate_non_repetitions_DLV("VI")
         assert_equal "Syntax error: 'D' letter cannot be repeated", validate_non_repetitions_DLV("DLD")
     end
+
+    def test_roman_range
+        assert_equal "Syntax error: Range must be 500 or less (but no minus than 0)", range_0_to_500("M")
+        assert_equal "All right", range_0_to_500("XX")
+        assert_equal "All right", range_0_to_500("D")
+        assert_equal "All right", range_0_to_500("LVII")
+    end
 end
