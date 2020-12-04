ac73dd450ea7b8dcf452f7ff8a84b253a58f732b
Victor Valdez <victor334_@hotmail.com>
2020-12-04 09:20:09 -0400

    non repetitions test and code added

--- romans.rb
+++ romans.rb
@@ -6,3 +6,30 @@ def validate_range(roman)
         message = "Ok"
     end
 end
+
+def validate_non_repetitions(roman)
+    chain = roman.split("")
+    first_letter = chain[0]
+    
+        v_times= 0
+        l_times = 0
+        for i in (0..chain.length - 1)
+            if chain[i] == "V"
+                v_times = v_times + 1
+                if v_times > 1
+                    message = "Syntax error"
+                else 
+                    message = "Ok"
+                end
+            elsif chain[i] == "L"
+                l_times = l_times + 1
+                if l_times > 1
+                    message = "Syntax error"
+                else
+                    message = "Ok"
+                end
+            end
+        end
+        return message     
+  
+end
--- romans_test.rb
+++ romans_test.rb
@@ -3,6 +3,13 @@ require "test/unit"
 
 class Tests < Test::Unit::TestCase
     def test_roman_range
-        assert_equal "Out of range", validate_range("MC")
+        assert_equal "Ok", validate_range("II")
+        assert_equal "Ok", validate_range("XL")
+        assert_equal "Ok", validate_range("L")
+        assert_equal "Ok", validate_range("XIII")
+    end
+
+    def test_non_repetitions_LV
+        assert_equal "Syntax error", validate_non_repetitions("XVV")
     end
 end
